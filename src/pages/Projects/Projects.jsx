4a903de6c2267f7b36e15e419af670b5a759bff7
MahadBuulle <mahadhussein012@gmail.com>
2023-10-06 20:03:51 +0300

    cpro

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -24,7 +24,7 @@ const Projects = () => {
                 <div className="card-image">
                   <span className="card-notify-badge">Full stack</span>
                   <img
-                    src="https://unctad.org/sites/default/files/2021-03/2021-03-15_eCommerceCOVID19report-1-1220x675px.jpg"
+                    src="https://scontent-mba1-1.xx.fbcdn.net/v/t1.15752-9/384563404_239799899065678_8000091403285438783_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=3hMd_qaNZSEAX_hfroh&_nc_ht=scontent-mba1-1.xx&oh=03_AdQRv4-JXvE82tfpo_s0XRqwDIsva4BSJDC100P9bQReSw&oe=65468A50"
                     alt="project1"
                   />
                 </div>
@@ -37,12 +37,12 @@ const Projects = () => {
                 <div className="card-body text-center">
                   <div className="ad-title m-auto">
                     <h6 className="text-uppercase">
-                      Techinfoyt Shopping Website
+                      Invoice generator project
                     </h6>
                   </div>
                   <a
                     className="ad-btn"
-                    href="https://github.com/techinfo-youtube/ecommerce-app-2023"
+                    href="https://github.com/MahadBulle/invoicegenProFrontAd.git"
                   >
                     View
                   </a>
@@ -52,50 +52,54 @@ const Projects = () => {
             <div className="col-md-4">
               <div className="card rounded">
                 <div className="card-image">
-                  <span className="card-notify-badge">Mobile App</span>
+                  <span className="card-notify-badge">Full stack</span>
                   <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR356D-1YtSagN4-_ZdjZ5H9o6PKUO4h12dvw&usqp=CAU"
+                    src="https://scontent-mba1-1.xx.fbcdn.net/v/t1.15752-9/384559594_986345802652244_917508593303299878_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=YoEz867oP3kAX-QaYnp&_nc_ht=scontent-mba1-1.xx&oh=03_AdRVTc9BCXrc7geagNoyalwAFeOIvV8PD6soD3xeRJiIXQ&oe=65465562"
                     alt="project2"
-                  />
-                </div>
-                <div className="card-image-overly m-auto mt-3">
-                  <span className="card-detail-badge">React Native</span>
-
-                  <span className="card-detail-badge">IOS / ANDROID</span>
-                </div>
-                <div className="card-body text-center">
-                  <div className="ad-title m-auto">
-                    <h5 className="text-uppercase">Techinfoyt Coding App</h5>
-                  </div>
-                  <a
-                    className="ad-btn"
-                    href="https://github.com/techinfo-youtube/ecommerce-app-2023"
-                  >
-                    View
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="card rounded">
-                <div className="card-image">
-                  <span className="card-notify-badge">Backend</span>
-                  <img
-                    src="https://www.nextwebi.com/assets/img/img-source/mobile-top-banner-28.png"
-                    alt="project1"
                   />
                 </div>
                 <div className="card-image-overly m-auto mt-3">
                   <span className="card-detail-badge">Node</span>
                   <span className="card-detail-badge">Express</span>
-
-                  <span className="card-detail-badge">NoSQL</span>
+                  <span className="card-detail-badge">react</span>
+                  <span className="card-detail-badge">Mongodb</span>
                 </div>
                 <div className="card-body text-center">
                   <div className="ad-title m-auto">
-                    <h5 className="text-uppercase">Techinfoyt Job Portal</h5>
+                    <h5 className="text-uppercase">betahouse admin</h5>
                   </div>
-                  <a className="ad-btn" href="view">
+                  <a
+                    className="ad-btn"
+                    href="https://beta-houses.vercel.app/dashboard/"
+                  >
+                    View
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card rounded">
+                <div className="card-image">
+                  <span className="card-notify-badge">Front-endweb</span>
+                  <img
+                    src="https://scontent-mba1-1.xx.fbcdn.net/v/t1.15752-9/384557869_855944256143039_5937637921703762869_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_ohc=YTlAa3dGdPwAX8BL4X7&_nc_ht=scontent-mba1-1.xx&oh=03_AdT6hUy4JXNOoqzXl-DAFGRhoRYGZplmsHqauSgtVdeR7g&oe=6546737F"
+                    alt="project1"
+                  />
+                </div>
+                <div className="card-image-overly m-auto mt-3">
+                  <span className="card-detail-badge">Reactjs</span>
+                  <span className="card-detail-badge">Material UI</span>
+
+                  <span className="card-detail-badge">TailwindCss</span>
+                </div>
+                <div className="card-body text-center">
+                  <div className="ad-title m-auto">
+                    <h5 className="text-uppercase">Beta small website</h5>
+                  </div>
+                  <a
+                    className="ad-btn"
+                    href="https://betaweb.vercel.app/"
+                  >
                     View
                   </a>
                 </div>
